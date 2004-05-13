f7406657a5c9e30ac8adc83eb17caf7778c6e1fd
cvs2git <>
2004-05-13 22:02:51 +0000

    This commit was manufactured by cvs2svn to create tag 'v1r5gr1'.
    Sprout from master 2004-05-13 22:02:49 UTC heather 'Initial import of new LdfConverter, replacing EbfConverter'
    Delete:
        cmt/.cvsignore
        cmt/requirements
        doc/.cvsignore
        doc/release.notes
        src/Dll/LdfConverter_dll.cxx
        src/Dll/LdfConverter_load.cxx
        src/ILdfCnvSvc.h
        src/LdfBaseCnv.cxx
        src/LdfBaseCnv.h
        src/LdfCalDigiCnv.cxx
        src/LdfCalDigiCnv.h
        src/LdfCnvSvc.cxx
        src/LdfCnvSvc.h
        src/LdfDiagnosticCnv.cxx
        src/LdfDiagnosticCnv.h
        src/LdfDigiCnv.cxx
        src/LdfDigiCnv.h
        src/LdfEventCnv.cxx
        src/LdfEventCnv.h
        src/LdfEventSelector.cxx
        src/LdfEventSelector.h
        src/LdfEventSummaryCnv.cxx
        src/LdfEventSummaryCnv.h
        src/LdfEvtIterator.h
        src/LdfTimeCnv.cxx
        src/LdfTimeCnv.h
        src/LdfTkrDigiCnv.cxx
        src/LdfTkrDigiCnv.h
        src/guiOptions.txt
        src/ldf2digi.txt
        src/mainpage.h
        src/test/LdfToRootOptions.txt
        src/test/jobOptions.txt
        src/test/test_LdfConverterAlg.cxx

--- src/Dll/LdfConverter_dll.cxx
+++
@@ -1,7 +0,0 @@
-/** @file LdfConverter_dll.cpp
-
-  $Header: /nfs/slac/g/glast/ground/cvs/EbfConverter/src/Dll/EbfConverter_dll.cxx,v 1.1.1.1 2003/03/24 15:45:02 heather Exp $
-*/
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(LdfConverter)
