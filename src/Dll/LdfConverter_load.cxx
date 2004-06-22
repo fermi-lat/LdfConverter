585d1d257ff889ad37294fefd82c9854c153c999
heather <>
2004-06-22 21:03:01 +0000

    add missing converters

--- src/Dll/LdfConverter_load.cxx
+++ src/Dll/LdfConverter_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/EbfConverter/src/Dll/EbfConverter_load.cxx,v 1.4 2003/12/19 07:43:00 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/LdfConverter/src/Dll/LdfConverter_load.cxx,v 1.1.1.1 2004/05/13 22:02:49 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -11,10 +11,12 @@
 DECLARE_FACTORY_ENTRIES(LdfConverter) {
   DECLARE_SERVICE(LdfCnvSvc);
   DECLARE_SERVICE( LdfEventSelector );
+  DECLARE_CONVERTER(LdfEventSummaryCnv);
   DECLARE_CONVERTER(LdfEventCnv);
   DECLARE_CONVERTER(LdfDigiCnv);
   DECLARE_CONVERTER(LdfTkrDigiCnv);
   DECLARE_CONVERTER(LdfCalDigiCnv);
+  DECLARE_CONVERTER(LdfAcdDigiCnv);
   DECLARE_CONVERTER(LdfDiagnosticCnv);
   DECLARE_CONVERTER(LdfTimeCnv);
 
