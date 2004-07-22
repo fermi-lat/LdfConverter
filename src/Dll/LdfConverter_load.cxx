2402e7010c9b304e2b888d06432c02d7fb236657
heather <>
2004-07-22 00:29:26 +0000

    activate ACD converter

--- src/Dll/LdfConverter_load.cxx
+++ src/Dll/LdfConverter_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/LdfConverter/src/Dll/LdfConverter_load.cxx,v 1.2 2004/06/22 21:03:00 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/LdfConverter/src/Dll/LdfConverter_load.cxx,v 1.3 2004/06/23 18:08:46 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -16,7 +16,7 @@ DECLARE_FACTORY_ENTRIES(LdfConverter) {
   DECLARE_CONVERTER(LdfDigiCnv);
   DECLARE_CONVERTER(LdfTkrDigiCnv);
   DECLARE_CONVERTER(LdfCalDigiCnv);
-//  DECLARE_CONVERTER(LdfAcdDigiCnv);
+  DECLARE_CONVERTER(LdfAcdDigiCnv);
   DECLARE_CONVERTER(LdfDiagnosticCnv);
   DECLARE_CONVERTER(LdfTimeCnv);
 
