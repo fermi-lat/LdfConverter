d3923c9282362132e1633a6e0f53eb66e5becdb2
ecephas <>
2008-12-09 00:08:32 +0000

    Check in LdfConverterLib.py for LdfConverter v4r3p2 (branch-tag, GlastReleasev15r49).

--- LdfConverterLib.py
+++ LdfConverterLib.py
@@ -1,7 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/LdfConverter/LdfConverterLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['LdfConverter'])
-	
+    env.Tool('ldfReaderLib')	
+    env.Tool('GlastSvcLib')
+    env.Tool('EventLib')
 def exists(env):
     return 1;
