5425ce5875f0dd4cf8764718fee208f07f21535b
jrb <>
2012-08-18 00:31:13 +0000

    support no-install headers on Windows

--- LdfConverterLib.py
+++ LdfConverterLib.py
@@ -1,4 +1,4 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/LdfConverter/LdfConverterLib.py,v 1.3 2009/01/23 00:20:58 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/LdfConverter/LdfConverterLib.py,v 1.4 2009/08/31 18:22:08 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['LdfConverter'])
@@ -6,5 +6,10 @@ def generate(env, **kw):
     env.Tool('GlastSvcLib')
     env.Tool('EventLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
+
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'AdfEvent') 
+        env.Tool('findPkgPath', package = 'LdfEvent') 
+        env.Tool('findPkgPath', package = 'EbfWriter') 
 def exists(env):
     return 1;
